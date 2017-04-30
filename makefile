8e95518fb720add656c2eb0f23b8f5cabdfbfe5d
tomek wesolowski <tik.wesolowski@wp.pl>
2017-04-30 12:10:29 +0200

    lista

---
+++ makefile
@@ -0,0 +1,2 @@
+zad_dom1: zad_dom1.c
+	gcc zad_dom1.c -o zad_dom1
---
+++ zad_dom1.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct List{
+    int value;
+    struct List *next;
+
+};
+
+void display(struct List *element){
+    
+    while(element){
+        if(element->value){
+            printf("wartosc -> %d \n", element->value);
+        }
+        element = element->next;
+    }
+    
+}
+
+void push(struct List *head, int value){
+    struct List *newElement;
+    struct List *cursor = head;
+    newElement = (struct List*)malloc(sizeof(struct List));
+    newElement->value = value;
+    
+    while(cursor->next){
+        
+        cursor = cursor->next;
+    }
+
+    cursor->next = newElement;
+    
+}
+
+
+int pop(struct List *head){
+
+    struct List *cursor = head;
+
+    while(cursor->next){
+        cursor = cursor->next;
+    }
+
+    int return_value = cursor->value;
+    free(cursor);
+    return return_value;
+}
+
+
+int main(){
+
+
+struct List *lista;
+
+
+lista = (struct List*)malloc(sizeof(struct List));
+lista->value = -1;
+
+for (int i=0; i< 15; i++){
+    push(lista, i);
+}
+display(lista);
+
+printf("-->%d", pop(lista));
+
+display(lista);
+
+
+free(lista);
+}
